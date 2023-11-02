09dd10429a94aa37756f55b0f8a7fb557ae0ce52
gabrielsantos1101 <gabriel.adv45@outlook.com>
2023-11-02 10:59:58 -0300

    feat: add tailwindcss-grid-areas plugin and update tailind config

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,35 +3,35 @@ import { Toggle } from '@/components/Toggle'
 export default function Home() {
   return (
     <main className="flex min-h-screen w-full flex-col items-center  justify-between p-12 md:p-12 lg:p-16 2xl:p-24">
-      <section className="w-full 2xl:w-4/5 grid-areas-md-layout lg:grid-areas-lg-layout grid h-screen gap-4">
-        <div className="grid-in-A flex items-center justify-center w-full rounded-xl bg-slate-300">
+      <section className="grid h-screen w-full gap-4 grid-areas-md-layout lg:grid-areas-lg-layout 2xl:w-4/5">
+        <div className="flex w-full items-center justify-center rounded-xl bg-slate-300 grid-in-A">
           1
         </div>
-        <div className="grid-in-B flex items-center justify-center rounded-xl bg-slate-500">
+        <div className="flex items-center justify-center rounded-xl bg-slate-500 grid-in-B">
           <Toggle />
         </div>
-        <div className="grid-in-C flex items-center justify-center rounded-xl bg-red-500">
+        <div className="flex items-center justify-center rounded-xl bg-red-500 grid-in-C">
           3
         </div>
-        <div className="grid-in-D rounded-xl bg-lime-300 flex justify-center items-center">
+        <div className="flex items-center justify-center rounded-xl bg-lime-300 grid-in-D">
           4
         </div>
-        <div className="grid-in-E flex items-center justify-center rounded-xl bg-cyan-400">
+        <div className="flex items-center justify-center rounded-xl bg-cyan-400 grid-in-E">
           5
         </div>
-        <div className="grid-in-F rounded-xl flex items-center justify-center bg-purple-500">
+        <div className="flex items-center justify-center rounded-xl bg-purple-500 grid-in-F">
           6
         </div>
-        <div className="grid-in-G rounded-xl flex items-center justify-center row-start-3 bg-white">
+        <div className="flex items-center justify-center rounded-xl bg-white grid-in-G">
           7
         </div>
-        <div className="grid-in-H rounded-xl flex items-center justify-center row-start-3 bg-orange-400">
+        <div className="flex items-center justify-center rounded-xl bg-orange-400 grid-in-H">
           8
         </div>
-        <div className="grid-in-I rounded-xl flex items-center justify-center bg-teal-500">
+        <div className="flex items-center justify-center rounded-xl bg-teal-500 grid-in-I">
           9
         </div>
-        <div className="grid-in-J rounded-xl flex items-center justify-center row-start-4 bg-blue-600">
+        <div className="flex items-center justify-center rounded-xl bg-blue-600 grid-in-J">
           10
         </div>
       </section>
