194738468108d2170570cf4de74f3149b99f5f4c
gabrielsantos1101 <gabriel.adv45@outlook.com>
2023-11-02 20:59:07 -0300

    refactor: change grid layout style

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,36 +2,36 @@ import { Toggle } from '@/components/Toggle'
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen w-full flex-col items-center  justify-between p-12 md:p-12 lg:p-16 2xl:p-24">
-      <section className="grid h-screen w-full gap-4 grid-areas-md-layout lg:grid-areas-lg-layout 2xl:w-4/5">
-        <div className="flex w-full items-center justify-center rounded-xl bg-slate-300 grid-in-A">
+    <main className="flex min-h-screen w-full  flex-col items-center  justify-between p-12 md:p-12 lg:p-16 2xl:p-24">
+      <section className="grid h-screen w-full grid-cols-sm-layout grid-rows-sm-layout gap-4 grid-areas-sm-layout md:grid-cols-md-layout md:grid-rows-md-layout md:grid-areas-md-layout lg:grid-cols-lg-layout lg:grid-rows-lg-layout lg:grid-areas-lg-layout 2xl:w-4/5">
+        <div className="flex items-center justify-center rounded-xl bg-slate-300 grid-in-A ">
           1
         </div>
-        <div className="flex items-center justify-center rounded-xl bg-slate-500 grid-in-B">
-          <Toggle />
+        <div className="flex items-center justify-center rounded-xl bg-slate-500 grid-in-B ">
+          2
         </div>
-        <div className="flex items-center justify-center rounded-xl bg-red-500 grid-in-C">
+        <div className="flex items-center justify-center rounded-xl bg-red-500 grid-in-C ">
           3
         </div>
-        <div className="flex items-center justify-center rounded-xl bg-lime-300 grid-in-D">
-          4
+        <div className="flex items-center justify-center rounded-xl bg-lime-300 grid-in-D ">
+          <Toggle />
         </div>
-        <div className="flex items-center justify-center rounded-xl bg-cyan-400 grid-in-E">
+        <div className="flex items-center justify-center rounded-xl bg-cyan-400 grid-in-E ">
           5
         </div>
-        <div className="flex items-center justify-center rounded-xl bg-purple-500 grid-in-F">
+        <div className="flex items-center justify-center rounded-xl bg-purple-500 grid-in-F ">
           6
         </div>
-        <div className="flex items-center justify-center rounded-xl bg-white grid-in-G">
+        <div className="flex items-center justify-center rounded-xl bg-white grid-in-G ">
           7
         </div>
-        <div className="flex items-center justify-center rounded-xl bg-orange-400 grid-in-H">
+        <div className="flex items-center justify-center rounded-xl bg-orange-400 grid-in-H ">
           8
         </div>
-        <div className="flex items-center justify-center rounded-xl bg-teal-500 grid-in-I">
+        <div className="flex items-center justify-center rounded-xl bg-teal-500 grid-in-I ">
           9
         </div>
-        <div className="flex items-center justify-center rounded-xl bg-blue-600 grid-in-J">
+        <div className="flex items-center justify-center rounded-xl bg-blue-600 grid-in-J ">
           10
         </div>
       </section>
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -18,8 +18,19 @@ module.exports = {
     extend: {
       colors: {},
       gridTemplateAreas: {
+        'sm-layout': ['A A ', 'B C', 'B D', 'E F', 'G H', 'I H', 'J J'],
         'md-layout': ['A A A', 'B C D', 'E F G', 'E H H', 'I I J'],
         'lg-layout': ['A A B C', 'D E F C', 'G G F H', 'I I J J'],
+      },
+      gridTemplateColumns: {
+        'sm-layout': 'repeat(2, minmax(0, 12.5rem))',
+        'md-layout': 'repeat(3, minmax(0, 20.625rem))',
+        'lg-layout': 'repeat(4, minmax(0, 20.625rem))',
+      },
+      gridTemplateRows: {
+        'sm-layout': 'repeat(7, minmax(9.375rem, 12.5rem))',
+        'md-layout': 'repeat(5, minmax(0, 20.625rem))',
+        'lg-layout': 'repeat(4, minmax(0, 20.625rem))',
       },
     },
   },
